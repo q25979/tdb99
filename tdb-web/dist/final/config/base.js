c702efc2f9d57ea39e954451f1eca63b7612daba
while <277161542@qq.com>
2019-07-26 16:56:01 +0800

    add

--- tdb-web/dist/final/config/base.js
+++ tdb-web/dist/final/config/base.js
@@ -2,5 +2,6 @@ module.exports = {
   appName: 'TDB',
   apiEndPoint: 'http://api.tdb99.com',
   // apiEndPoint: 'http://127.0.0.1:5000',
-  cdn: '//xiaoyu168.oss-ap-southeast-1.aliyuncs.com',
+  // cdn: '//xiaoyu168.oss-ap-southeast-1.aliyuncs.com',
+  cdn: '//tdb99.oss-cn-beijing.aliyuncs.com',
 };
