248488b6a844999bb8cc86cb5e9727467d935ff1
while <277161542@qq.com>
2019-08-02 16:29:39 +0800

    1

--- cordovaApp/platforms/ios/www/cordova_plugins.js
+++ cordovaApp/platforms/ios/www/cordova_plugins.js
@@ -45,8 +45,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-camera": "4.1.0",
-  "cordova-plugin-statusbar": "2.4.3",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-statusbar": "2.4.3"
 };
 // BOTTOM OF METADATA
 });
